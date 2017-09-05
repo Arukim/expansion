dc79c4bc1651ae945e74af2a724514a3fe8038de
Nikita Smelov <Nikita_Smelov@epam.com>
2017-09-05 14:02:17 +0300

    forces map parsing added

---
+++ models/map.go
@@ -0,0 +1,40 @@
+package models
+
+import "fmt"
+
+type Map struct {
+	Size  int
+	Width int
+	Data  []int
+}
+
+// NewMap creates new map instance
+func NewMap(width int) *Map {
+	size := width * width
+	m := Map{
+		Width: width,
+		Size:  size,
+		Data:  make([]int, size),
+	}
+	return &m
+}
+
+func (m *Map) Print() {
+	for i := m.Width - 1; i >= 0; i-- {
+		fmt.Printf("%v\n", m.Data[i*m.Width:i*m.Width+m.Width])
+	}
+}
+
+func (m *Map) Iterate(f func(int, int)) {
+	for i := 0; i < m.Size; i++ {
+		f(i, m.Data[i])
+	}
+}
+
+func (m *Map) Get(p Point) int {
+	return m.Data[p.GetPos(m.Width)]
+}
+
+func (m *Map) Set(p Point, value int) {
+	m.Data[p.GetPos(m.Width)] = value
+}
